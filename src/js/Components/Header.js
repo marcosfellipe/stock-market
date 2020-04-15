979bdc22e44dfa431eeb1c1a97b8f0042de139ce
Marcos Fellipe <marcosdeda93@gmail.com>
2020-04-15 17:18:59 -0300

    Separar menu do header

--- src/js/Components/Header.js
+++ src/js/Components/Header.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import '../../css/Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import Menu from './Menu';
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+        this._menu = null;
+    }
+
+    componentDidMount = () => {
+        this._menu = document.getElementById('menu');
+    }
 
     menuToggle = () => {
-        console.log('click');
+        if (this._menu.classList.contains('nav-menu-open')) {
+            this._menu.classList.remove('nav-menu-open');
+        } else {
+            this._menu.classList.add('nav-menu-open');
+        }
     }
 
     render() {
@@ -15,13 +28,7 @@ class Header extends Component {
             <header>
                 <h1 className="header">Stock Market<FontAwesomeIcon icon={faBars} size="xs" onClick={this.menuToggle} /></h1>
             </header>
-            <nav className="nav-menu">
-                <ul>
-                    <li>Home</li>
-                    <li>Configurações</li>
-                    <li>Sobre</li>
-                </ul>
-            </nav>
+            <Menu close={this.menuToggle} />
             </>
         );
     }
--- src/js/Components/Menu.js
+++ src/js/Components/Menu.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import '../../css/Menu.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes, faHome, faCog, faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
 
 function Menu(props) {
     return (
-        <nav>
-            <ul>
-                <li></li>
-            </ul>
-        </nav>
+        <div id="menu" className="nav-menu">
+            <div className="nav-menu-close">
+                <FontAwesomeIcon icon={faTimes} size="lg" onClick={props.close} />
+            </div>
+            <nav>
+                <ul className="nav-menu-list">
+                    <li className="menu-list-item">
+                        <FontAwesomeIcon icon={faHome} size="lg" />
+                        <span className="list-item-text">Home</span>
+                    </li>
+                    <li className="menu-list-item">
+                        <FontAwesomeIcon icon={faCog} size="lg" />
+                        <span className="list-item-text">Configurações</span>
+                    </li>
+                    <li className="menu-list-item">
+                        <FontAwesomeIcon icon={faQuestionCircle} size="lg" />
+                        <span className="list-item-text">Sobre</span>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     );
 }
 
